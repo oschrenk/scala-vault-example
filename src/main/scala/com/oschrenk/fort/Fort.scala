01ba61a98b5980086b46e2bdda335d5e039dffc5
Oliver Schrenk <oliver.schrenk@gmail.com>
2017-10-30 12:04:27 +0100

    Read secret,role from application.conf

--- src/main/scala/com/oschrenk/fort/Fort.scala
+++ src/main/scala/com/oschrenk/fort/Fort.scala
@@ -1,9 +1,22 @@
 package com.oschrenk.delight
 
+import com.typesafe.config.ConfigFactory
+
 object Fort {
 
   def main(args: Array[String]): Unit = {
     println("Hello World")
+    println(Config.vault)
   }
+
+}
+
+object Config {
+
+  private val config = ConfigFactory.load()
+
+  val vault = config.getString("vault")
+  val roleId = config.getString("role_id")
+  val secretId = config.getString("secret_id")
 
 }
